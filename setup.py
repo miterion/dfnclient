5ff8e0de0c8e87edc2536923f14a01ceaa83283b
Heiko Carrasco <heiko.carrasco@yahoo.com>
2018-09-28 19:06:57 +0200

    Added url to setup py

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r") as fh:
 
 setup(
         name='dfnclient',
-        version='0.0.3',
+        version='0.0.4',
         license='MIT',
         description='Certificate client based on the soap API of the dfn',
         install_requires =['click==7.0', 'termcolor==1.1.0', 'suds-py3==1.3.3.0'],
         packages=find_packages(),
         long_description=long_description,
         long_description_content_type="text/markdown",
+        url='https://github.com/Miterion/dfnclient',
         entry_points={
             'console_scripts': [
                 'dfnclient=dfngen.dfnclient:cli',
